ada6c1ad6a5bd08fd4079c31171062a1cc392c70
wilykun1994 <wilykun1994@gmail.com>
2022-06-25 13:56:19 +0700

    kwokowkwowkwkk

---
+++ connect/language/help.js
@@ -0,0 +1,113 @@
+exports.help = (prefix, author, botname, owner, wib, jangwak) => {
+return `
+_*=>=>=>=>=>=>=>=>=>=>=>*_
+*( BOT WA MULTI DEVICE )*
+_*=>=>=>=>=>=>=>=>=>=>=>*_
+❑ _*MY SERVER BOT INFO*_
+*× Owner Name : ${global.author}*
+*× Bot Name: ${global.botname}*
+*× My Owner : ${global.owner}*
+*× My Team : Neoxy Team*
+*× Database : MongoDB*
+*× Library : BaileysMD*
+*× Time : ${wib}*
+*× Tanggal : ${jangwak}*
+_*=>=>=>=>=>=>=>=>=>=>=>*_
+
+*∆ [ SIMPEL BOT MENU ] ∆*
+
+❑ *OTHERS MENU*
+ • #ping
+ • #limit
+ • #script
+ • #speed
+ • #status
+ • #runtime
+ • #ceklimit
+ • #botstatus
+
+❑ *GROUP MENU*
+ • #kick
+ • #add
+ • #vote
+ • #tagall
+ • #group
+ • #upvote
+ • #devote
+ • #cekvote
+ • #delvote
+ • #hidetag
+ • #setname
+ • #setdesc
+ • #editinfo
+ • #antilink
+ • #demote
+ • #promote
+ • #setppgrup
+
+❑ *OWNER MENU*
+ • #join
+ • #bcall
+ • #bcgc
+ • #leave
+ • #getcase
+ • #creategc
+ • #block
+ • #unblock
+ • #broadcast
+ • #prem del 628xxxx
+ • #prem add 628xxxx
+
+❑ *FUN MENU*
+ • #math
+ • #tebak
+ • #jadian
+ • #delttt
+ • #suitpvp
+ • #jodohku
+ • #tictactoe
+ • #family100
+
+❑ *CONVERT MENU*
+ • #attp
+ • #tourl
+ • #ssweb
+ • #tahta
+ • #teksto
+ • #sticker
+ • #toimage
+ • #tovideo
+ • #tomp3
+
+❑ *DOWNLOAD MENU*
+ • #play
+ • #tiktok
+ • #ytplay
+ • #ytmp3
+ • #ytmp4
+ • #tiktokmp4
+ • #tiktokmp3
+ • #mediafire
+
+❑ *SEARCH MENU*
+ • #ytsearch
+ • #pinterest
+ • #wallpaper
+ • #wikimedia
+ • #quotesanime
+
+❑ *NULIS MENU*
+ • #nulis
+ • #foliokiri
+ • #nuliskiri
+ • #nuliskanan
+ • #foliokanan
+
+❑ *ANIME MENU*
+ • #nekonime
+ • #randomloli
+ • #randomneko
+ • #randomwaifu
+ • #randomanime
+`
+}
